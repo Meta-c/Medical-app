a91b025a3a2fb7c4fb54ec49e21222b5093a7438
Meta-c <metamedhat116@gmail.com>
2022-11-28 08:16:25 +0200

    removed device preview

--- knowledge_system/lib/main.dart
+++ knowledge_system/lib/main.dart
@@ -1,15 +1,12 @@
 // import 'package:ecommerce/core/dependecies.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:device_preview/device_preview.dart';
 import 'package:knowledge_system/core/dependecies.dart';
 import 'features/splash/splash_view.dart';
 
 void main() {
   setupDependecies();
-  runApp(DevicePreview(
-      enabled: !kReleaseMode, builder: (context) => const diseasIdentify()));
+  runApp(const diseasIdentify());
 }
 
 class diseasIdentify extends StatelessWidget {
@@ -21,8 +18,8 @@ class diseasIdentify extends StatelessWidget {
       navigatorKey: Get.key,
       theme: ThemeData(fontFamily: 'Poppins'),
       useInheritedMediaQuery: true,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
+      // locale: DevicePreview.locale(context),
+      // builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       home: const SplashView(),
     );
